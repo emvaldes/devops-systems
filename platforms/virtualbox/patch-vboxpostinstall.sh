abd46c8d0fdec3d9b3e8bd15145f9b5521de2ae7
Eduardo Valdes Jr <67751328+takeda-netsrv-eduardovaldes@users.noreply.github.com>
2023-01-06 23:24:11 -0700

    Publishing VirtualBox Infrastructure Deployment patch-files.

---
+++ platforms/virtualbox/patch-vboxpostinstall.sh
@@ -0,0 +1,23 @@
+--- ./default-vboxpostinstall.sh	2023-01-06 22:42:02
++++ ./custom-vboxpostinstall.sh	2023-01-06 22:43:43
+@@ -206,6 +206,10 @@
+ log_command_in_target apt-get -y install build-essential
+ log_command_in_target apt-get -y install linux-headers-$(uname -r)
+
++## emvaldes@hotmail.com
++log_command_in_target apt-get -y install openssh-server
++log_command_in_target systemctl enable ssh
++log_command_in_target systemctl status ssh
+
+ #
+ # GAs
+@@ -220,7 +224,8 @@
+ MY_IGNORE_EXITCODE=
+ log_command_in_target usermod -a -G vboxsf "devops"
+
+-
++## emvaldes@hotmail.com
++log_command_in_target usermod -a -G sudo "devops"
+
+ #
+ # Test Execution Service.
