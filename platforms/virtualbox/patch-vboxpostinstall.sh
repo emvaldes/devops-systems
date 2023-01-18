23198a5b618f76c5991130b22f06b22fa8d462ee
Eduardo Valdes Jr <67751328+takeda-netsrv-eduardovaldes@users.noreply.github.com>
2023-01-18 13:32:26 -0700

    Just removing some short-code switches to long-code

--- platforms/virtualbox/patch-vboxpostinstall.sh
+++ platforms/virtualbox/patch-vboxpostinstall.sh
@@ -1,13 +1,13 @@
 --- ./default-vboxpostinstall.sh	2023-01-06 22:42:02
 +++ ./custom-vboxpostinstall.sh	2023-01-06 22:43:43
 @@ -206,6 +206,10 @@
- log_command_in_target apt-get -y install build-essential
- log_command_in_target apt-get -y install linux-headers-$(uname -r)
+ log_command_in_target apt-get install build-essential --yes
+ log_command_in_target apt-get install linux-headers-$(uname -r) --yes
 
 +## emvaldes@hotmail.com
-+log_command_in_target apt-get -y install openssh-server
-+log_command_in_target systemctl enable ssh
-+log_command_in_target systemctl status ssh
++log_command_in_target apt-get install openssh-server --yes;
++log_command_in_target systemctl enable ssh  ## 2>/dev/null;
++log_command_in_target systemctl status ssh  2>/dev/null;
 
  #
  # GAs
