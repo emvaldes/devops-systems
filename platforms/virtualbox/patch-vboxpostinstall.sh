d1c9516c381321c701cc3200138d8042ed64326e
Eduardo Valdes Jr <67751328+takeda-netsrv-eduardovaldes@users.noreply.github.com>
2023-01-18 21:02:25 -0700

    Correcting this issue in auto-generated templates and expanded on pre-installed packages.

--- platforms/virtualbox/patch-vboxpostinstall.sh
+++ platforms/virtualbox/patch-vboxpostinstall.sh
@@ -1,23 +1,28 @@
---- ./default-vboxpostinstall.sh	2023-01-06 22:42:02
-+++ ./custom-vboxpostinstall.sh	2023-01-06 22:43:43
-@@ -206,6 +206,10 @@
- log_command_in_target apt-get install build-essential --yes
- log_command_in_target apt-get install linux-headers-$(uname -r) --yes
+--- ./default-vboxpostinstall.sh	2023-01-01 00:00:00
++++ ./custom-vboxpostinstall.sh	  2023-01-01 00:00:00
+@@ -206,7 +206,16 @@
+ log_command_in_target apt-get -y install build-essential
+ log_command_in_target apt-get -y install linux-headers-$(uname -r)
+
++## emvaldes@hotmail.com
++log_command_in_target apt-get install git --yes;
++log_command_in_target apt-get install jq --yes;
++log_command_in_target apt-get install python-is-python3 --yes;
 
 +## emvaldes@hotmail.com
 +log_command_in_target apt-get install openssh-server --yes;
 +log_command_in_target systemctl enable ssh  ## 2>/dev/null;
-+log_command_in_target systemctl status ssh  2>/dev/null;
-
++log_command_in_target systemctl status ssh  ## 2>/dev/null;
++
  #
  # GAs
-@@ -220,7 +224,8 @@
+ #
+@@ -220,6 +229,8 @@
  MY_IGNORE_EXITCODE=
  log_command_in_target usermod -a -G vboxsf "{{ service-account }}"
 
--
 +## emvaldes@hotmail.com
 +log_command_in_target usermod -a -G sudo "{{ service-account }}"
 
+
  #
- # Test Execution Service.
